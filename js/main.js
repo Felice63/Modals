92ef63277afc8b5b3555f47e941a9d2e27a1fd39
Felice63 <fortefelice63@gmail.com>
2023-01-05 17:10:36 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// 1] Query all the elements with class="show-modal" using the querySelectorAll() method and store them in a variable named 'showModal'. This generates an iterable object called a 'NODELIST'
+
+// uerySelectorAll() and getElementsByTagName() are not JavaScript methods. They are part of the browser APIs that let you access DOM elements. You can then manipulate them with JavaScript.
+
+const showModal = document.querySelectorAll(".show-modal");
+
+// 2] I used the 'for/of' method instead of the forEach() method to iterate over each nodelist becasue it gets values instead of properties.
+// 'for/of' works on any iterable object.
+// The forEach() method is typically used with arrays because it works on the indvidual indices of the array. This is the same with the for/in(method) which is also used with indices and numeric keys.
+
+// The main difference between NodeLists vs. Arrays is that NodeLists are a way to access DOM elements. The DOM is an API separate from JavaScript, and nodelists are not part of JavaScript.  Other languages can access the DOM besides JavaScript.
+
+// Arrays, however, are a specific JavaScript object that contains indexed collections of data types. A NodeList may look like an array, but it is not. You can loop through a NodeList and refer to its nodes by index. But, you cannot use Array methods like push(), pop(), or join() on a NodeList.
+
+// To show the Modal Pop-up window, the 'el' parameter represents the individual instance of each 'showModal' depending on which one is clicked. 'el' could be named anything. An arrow function is being used for simplicity.
+
+for (const el of showModal)
+  el.addEventListener("click", () => {
+    const getModal = document.querySelector(".modal");
+    const addOpen = document.createAttribute("open");
+
+    // Conditional to check whether the dialog element has the 'open' attribute and if not, then add it
+    if (getModal.hasAttribute("open") === false) {
+      getModal.setAttributeNode(addOpen);
+    }
+
+    // Define and capture the info from the data-* attributes in the HTML file
+    // Remember, 'el' represents each individual instance of a clickable element
+    const projName = el.dataset.name;
+    const projDescript = el.dataset.description;
+    const projImg = el.dataset.img;
+
+    // console.log(projName, projDescript, projImg);
+    // Store the 'addInfo' class in a variable
+    const addTheInfo = document.querySelector(".addInfo");
+
+    // Write the info to the pop-up
+    return (addTheInfo.innerHTML = `
+
+  <h2>The ${projName}</h2>
+  <img src="${projImg}">
+  <p>${projDescript}</p>
+  `);
+  });
+
+// 4] Get the information about the project from the HTML data-* attributes in the <section> elements with class='show-modal'
+
+// 5] Add it to the <section> element with class="addInfo", which is inside the <dialog> element. The dialog element is the pop-up modal
+
+// https://css-tricks.com/a-bunch-of-options-for-looping-over-queryselectorall-nodelists/
